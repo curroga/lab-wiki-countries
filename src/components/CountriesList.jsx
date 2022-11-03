a6b978054412370c1d151ee803be08cab18c3e53
Curro Garcia <curro-91@hotmail.com>
2022-11-04 00:20:08 +0100

    lab done con axios

--- src/components/CountriesList.jsx
+++ src/components/CountriesList.jsx
@@ -1,33 +1,58 @@
-import React, { useEffect, useState } from 'react'
-import { Link } from 'react-router-dom'
-import countries from '../countries.json'
+import { Link } from 'react-router-dom';
+import countries from '../countries.json';
+import { useState, useEffect } from 'react'
+
+import axios from 'axios'
 
 function CountriesList() {
-  //console.log(countries[0].alpha3Code)
-   const [countriesList, setCountriesList] = useState([])
 
-   useEffect(() => {
-    
-    setCountriesList(countries)
-   }, [])
+  const [paisesList, setPaisesList] = useState(null)
+  const [isFetching, setIsFetching] = useState(true)
+
+  useEffect(() => {
+    setIsFetching(true)
+    axios.get('https://ih-countries-api.herokuapp.com/countries')
+    .then((response) => {
+      //console.log(response.data)
+      setPaisesList(response.data)
+      setIsFetching(false)
+
+    })
+    .catch((err) => {
+      console.log(err)
+    })
+  },[])
+
+  if(isFetching === true) {
+    return <h3>... buscando</h3>
+  }
+
+
 
   return (
-    <div>
-
-      <h5>Lista de paises</h5>
-
-      {countriesList.map((eachCountrie) => {
-        return(
-          <p key ={eachCountrie.alpha2Code}>
-          <Link to={`/country/${eachCountrie.alpha3Code}`}>{eachCountrie.name.official}</Link>
-          
-        </p>
-        )
-      })}
-
-    
+    <div className="container">
+      <div className="row">
+        <div
+          className="col-5"
+          style={{ maxHeight: '90vh', overflow: 'scroll' }}
+        >
+          {paisesList.map((eachPaises, index) => {
+            return (
+              <div className="list-group">
+                <Link
+                  key={eachPaises.alpha2Code + index}
+                  className="list-group-item list-group-item-action"
+                  to={`/country/${eachPaises.alpha3Code}`}
+                >
+                  {eachPaises.name.official}
+                </Link>
+              </div>
+            );
+          })}
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default CountriesList
+export default CountriesList;
--- src/components/CountryDetails.jsx
+++ src/components/CountryDetails.jsx
@@ -1,57 +1,59 @@
-import React from 'react'
-import CountriesList from './CountriesList'
-import countries from '../countries.json'
-import { useParams } from 'react-router-dom'
-
+import React from 'react';
+import CountriesList from './CountriesList';
+import countries from '../countries.json';
+import { useParams } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 
 function CountryDetails() {
+  const { alpha3Code } = useParams();
 
-  const {alpha3Code} = useParams()
-  
   //console.log(countries)
   const pais = countries.filter((eachCountrie) => {
-    if(alpha3Code === eachCountrie.alpha3Code){
-      return true
-    }else {
-      return false
+    if (alpha3Code === eachCountrie.alpha3Code) {
+      return true;
+    } else {
+      return false;
     }
-  })
-  console.log(pais[0])
+  });
+  console.log(pais);
   return (
-
-    <div>
-
-      <div id='country-container'>
-
+    <div className="d-flex">
       <CountriesList />
 
-      <div>
-        
-        <p>Nombre:{pais[0].name.official}</p>
-        <p>Capital:{pais[0].capital[0]}</p>
-        <p>Area:{pais[0].area}km^2</p>
-
-        <div>
-        <p>Borders: </p>
-        {pais[0].borders.map((eachBorder) => {
-          return(
-            <ul>
-              <li>{eachBorder}</li>
-
-            </ul>
-          )
-        })}
-        </div>
-
+      <div className="col-7">
+        <h1>{pais[0].name.official}</h1>
+        <table className="table">
+          <thead></thead>
+          <tbody>
+            <tr>
+              <td style={{ width: '30%' }}>Capital</td>
+              <td>{pais[0].capital[0]}</td>
+            </tr>
+            <tr>
+              <td>Area</td>
+              <td>
+                {pais[0].area}km
+                <sup>2</sup>
+              </td>
+            </tr>
+            <tr>
+              <td>Borders</td>
+              <td>
+                {pais[0].borders.map((eachBorder) => {
+                  return (
+                    <ul>
+                      <Link>{eachBorder}</Link>
+                    </ul>
+                  );
+                })}
+              </td>
+            </tr>
+          </tbody>
+        </table>
       </div>
-      
-
-      </div>
-
-
     </div>
-  )
+  );
 }
 
-export default CountryDetails
+export default CountryDetails;
